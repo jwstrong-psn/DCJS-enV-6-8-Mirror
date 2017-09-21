53a3ffe23ee53b5761917c0336fcf371b9435a8d
Strong, Joseph <Joseph.Strong@pearson.com>
2017-09-21 12:30:47 -0700

    Original Master CJS from enV68 Primary SCO Master

---
+++ Desmos 6-8 Test Master SCO CJS.js
@@ -0,0 +1,26 @@
+window.PearsonGL = window.PearsonGL || {};
+window.PearsonGL.External = window.PearsonGL.External || {};
+
+/******************************************************************************
+* Module: masterJS 
+******************************************************************************/
+PearsonGL.External.masterJS = (function() {
+  
+  /***********************************************************************************
+   * PRIVATE VARIABLES / FUNCTIONS
+   **********************************************************************************/
+  var exports = {};  // This object is used to export public functions/variables
+  
+  
+  /***********************************************************************************
+   * EXPORTS / PUBLIC FUNCTIONS
+   *
+   * To declare your function
+   * exports.myFunc1 = function() {};
+   * exports.myFunc2 = function() {};
+  **********************************************************************************/
+
+    console.log("Master CJS loaded");
+  return exports;
+})();
+
